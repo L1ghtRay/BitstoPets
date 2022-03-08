4cefe24752124c21c0d63e834fffacb663460a60
L1ghtRay <88073630+L1ghtRay@users.noreply.github.com>
2022-03-09 00:46:14 +0530

    Auction Data Fetcher and Sorter Complete

--- BitstoPets.py
+++ BitstoPets.py
@@ -8,10 +8,35 @@ import json
 
 pets = ['Ammonite','Armadillo','Baby Yeti','Bal','Bat','Bee','Black Cat','Blaze','Blue Whale','Chicken','Dolphin','Elephant','Ender Dragon','Enderman','Endermite','Flying Fish','Ghoul','Giraffe','Golden Dragon','Golem','Grandma Wolf','Griffin','Guardian','Horse','Hound','Jellyfish','Jerry','Lion','Magma Cube','Megoladon','Mithril Golem','Monkey','Ocelot','Parrot','Phoenix','Pig','Pigman','Rabbit','Rat','Rock','Scatha','Sheep','Silverfish','Skeleton','Skeleton Horse','Snowman','Spider','Spirit','Squid','Tarantula','Tiger','Turtle','Wither Skeleton','Wolf','Zombie']
 
-data = requests.get("https://api.hypixel.net/skyblock/auctions")
-d = data.json()
+user = str(input("Enter the name of your User (From C:\\Users\\ Folder): "))
 
-sort = [items for items in d["auctions"] if items["bin"] and items["category"] == "misc" and "[Lvl" in items["item_name"]]
+url = "https://api.hypixel.net/skyblock/auctions?page="
+ini = requests.get(url+"0")
+ini_d = ini.json()
+totalpg = ini_d['totalPages']
+print('Total Number of Pages:',totalpg)
+x = []
+for a in range(totalpg):
+    data = requests.get(url+str(a))
+    d = data.json()
+    if d['success'] == False:
+        data = requests.get(url+str(a))
+        d = data.json()
+        print("Fail 1")
+        if d['success'] == False:
+            data = requests.get(url+str(a))
+            d = data.json()
+            print("Fail 2")
+            if d['success'] == False:
+                data = requests.get(url+str(a))
+                d = data.json()
+                print("Fail 3")
+    x.extend(d["auctions"])
+    print("Page:", a)
 
-with open("F:\Coding\Python\BitstoPets\output.txt", "w") as f:
+sort = [items for items in x if not items['claimed'] and items["bin"] and "[Lvl" in items["item_name"]]
+
+with open("C:\\Users\\" + user + "\\output.txt", "w") as f:
     print(str(sort).encode("ascii","ignore").decode(), file=f)
+
+print("Number of Pet Bins:",len(sort))
